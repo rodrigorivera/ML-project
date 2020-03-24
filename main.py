7f3bc7fad721635cee1a0d9f1ca39ae236996008
NktBkzn <46127313+NktBkzn@users.noreply.github.com>
2020-03-24 19:45:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from M5_Competition.preprocessing import preprocess
+from M5_Competition.theta_method.ses_theta import sesThetaF
+from M5_Competition.theta_method.general_theta import sThetaF
+
+
+FORECAST_LENGTH = 28
+
+path = 'data/'
+calendar = pd.read_csv(path+'calendar.csv')
+sales = pd.read_csv(path+'sales_train_validation.csv')
+# sample = pd.read_csv(path+'sample_submission.csv')
+# sell_prices = pd.read_csv(path+'sell_prices.csv')
+
+sales = preprocess(sales, flag_trim=True, history_period=730)
+
+dim_vert, dim_hor = sales.shape
+forecast_df = pd.DataFrame(np.zeros((dim_vert*2, FORECAST_LENGTH+1)),
+                           columns=['id'] + ['F' + str(i + 1) for i in range(FORECAST_LENGTH)])
+ids = np.concatenate((sales['id'].values, sales['id'].str.replace('_validation', '_evaluation')))
+forecast_df['id'] = ids
+
+sales.set_index('id', inplace=True)
+
+dates_sales = calendar.loc[calendar.d.isin(sales.columns), 'date'].values
+for index, row in sales.iterrows():
+    data = pd.DataFrame(row.values, index=dates_sales)
+    forecast = sThetaF(data, s_period=7, h=28)
+    forecast_df.loc[forecast_df.id==index, :] = np.concatenate(([index], forecast['mean'].flatten()))
+
+forecast_df.to_csv('Theta.Nikita.csv', index=False)
---
+++ preprocessing.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+
+def preprocess(sales, flag_trim=True, history_period=730):
+    columns_to_del = ['item_id', 'dept_id', 'cat_id', 'store_id', 'state_id']
+    sales.drop(columns_to_del, axis=1, inplace=True)
+    if flag_trim:
+        # trim the time series
+        prefix, last_date = sales.columns[-1][:2], int(sales.columns[-1][2:])
+        start_date = last_date - history_period
+        # find closest date that exists in sales df
+        while prefix+str(start_date) not in sales.columns:
+            start_date -= 1
+
+        sales = pd.concat([sales.iloc[:, 0], sales.loc[:, prefix+str(start_date):]], axis=1)
+
+    # sales = reduce_mem_usage(sales)
+
+    return sales
